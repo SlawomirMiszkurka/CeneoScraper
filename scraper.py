a114f12661bb412c5d2de73ae32395381807f501
SlawomirMiszkurka <s216356@student.uek.krakow.pl>
2022-04-10 12:12:28 +0200

    2022-04-10 SM

--- scraper.py
+++ scraper.py
@@ -3,10 +3,13 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-def extract_element(ancestor, selector):
+def extract_element(ancestor, selector, attribute=None):
     try:
-        return ancestor.select(selector).pop(0).text.strip()
-    except: return None
+        if attribute:
+            return ancestor.select(selector).pop(0)[attribute].strip()
+        else:
+            return ancestor.select(selector).pop(0).text.strip()
+    except IndexError: return None
 product_id = input("Podaj identyfikator produktu: ")
 url_pre = "https://www.ceneo.pl"
 url_post = "/opinie-"
@@ -18,36 +21,36 @@ all_reviews = []
 while(page_no):
     url = url_pre+product_id+url_post+str(page_no)
     response= requests.get(url, allow_redirects=False)
-        if  response.status_code == requests.codes.ok:
+    if  response.status_code == requests.codes.ok:
         page_dom = BeautifulSoup(response.text, 'html.parser')
         reviews = page_dom.select("div.js_product-review")
         for review in reviews:
             review_id = review["data-entry-id"]
-            author = review.select("span.user-post__author-name").pop(0).text.strip()
-            try:
-                recommendation = review.select("span.user-post__author-recomendation > em").pop(0).text
-                recommendation = True if recommendation == "Polecam" else False
-            except: recommendation == None
-            stars = review.select("span.user-post__score-count").pop(0).text
-            stars = float(stars.split("/").pop(0).replace(",","."))
-            content = review.select("div.user-post__text").pop(0).get_text()
-            content = content.replace("\n"," ").replace("  "," ").strip()
-            publish_date = review.select("span.user-post__published > time:nth-child(1)").pop(0)["datetime"]
-            publish_date = publish_date.split(" ").pop(0)
-            try:
-                purchase_date = review.select("span.user-post__published > time:nth-child(1)").pop(0)["datetime"]
-                purchase_date = purchase_date.split(" ").pop(0)
-            except IndexError: purchase_date = None
-            useful = review.select("button.vote-yes >span").pop(0).text
-            useful = int(useful)
-            useless = review.select("button.vote-no >span").pop(0).text
-            useless = int(useless)
+            author = extract_element(review,"span.user-post__author-name")
+            recommendation = extract_element(review,"span.user-post__author-recomendation > em")
+            stars = extract_element(review,"span.user-post__score-count")
+            content = extract_element(review,"div.user-post__text")
+            publish_date = extract_element(review,"span.user-post__published > time:nth-child(1)")
+            purchase_date = extract_element(review,"span.user-post__published > time:nth-child(2)","datetime")
+            useful = extract_element(review,"span[id^=vote-yes]")
+            useless = extract_element(review"span[id^=vote-no]")
+
             pros = review.select("div.review-feature__title--positives ~ div.review-feature__item")
             pros = [item.text.strip() for item in pros]
             pros = ", ".join(pros)
+
             cons = review.select("div.review-feature__title--positives ~ div.review-feature__item")
             cons = [item.text.strip() for item in cons]
             cons = ", ".join(cons)
+
+            recommendation = True if recommendation == "Polecam" else False if recommendation == "Nie polecam" else None
+            stars = float(stars.split("/").pop(0).replace(",","."))
+            content = content.replace("\n"," ").replace("  "," ").strip()
+            publish_date = publish_date.split(" ").pop(0)
+                purchase_date = purchase_date.split(" ").pop(0)
+            useful = int(useful)
+            useless = int(useless)
+
             single_review = {
                 "review_id": review_id,
                 "author": author,
